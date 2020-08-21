ed14afffd9ff9d4aff3d4707428c67f4a44d0d0f
snalwyhh <63695514+snalwyhh@users.noreply.github.com>
2020-08-21 13:36:38 +0700

    file utama

---
+++ contoh class dan objek.php
@@ -0,0 +1,11 @@
+<?php
+    class Makanan{
+        public $Pedas;
+        public $Manis;
+
+        function Mengenyangkan(){
+            return "Nyam Nyam Nyam";
+        }
+    }
+    $seblak = new Makanan();
+?>
